41bdfb7e648d78779f2741ac0a76719b3c01b38a
Avinash Varna <avinash.varna@gmail.com>
2017-11-24 23:48:13 -0700

    Updated setup.py to indicate python 3 support

--- setup.py
+++ setup.py
@@ -17,7 +17,10 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='sanskrit',
 
